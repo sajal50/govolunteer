f4cf73f3fc7dab3b2e464869372c5380da58147b
Tushar Agarwal <tusharagarwal@Tushars-MacBook-Air.local>
2016-12-15 20:49:44 -0500

    Updated applicatio.py, pushed database

--- api/application.py
+++ api/application.py
@@ -6,7 +6,7 @@ import time
 import config as Config
 from flask import Flask
 from pprint import pprint
-from flask import Flask, request, render_template, g, redirect, Response, make_response, jsonify, url_for, session
+from flask import Flask, request, render_template, g, redirect, Response, make_response, jsonify, url_for
 from flaskext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 
@@ -22,22 +22,11 @@ app.config['MYSQL_DATABASE_DB'] = 'govol'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql = MySQL(app)
 '''mysql.init_app(app)'''
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
 @app.route('/')
 @app.route('/index')
 def index():
-    if 'username' in session:
-        return 'Logged in as %s' % escape(session['username'])
     return render_template('index.html')
-
-@app.route('/api/setSession/<vari>', methods=['POST'])
-def setSession(vari):
-    session['username']=vari;
-    return json.dumps({'message':'Session variable has been set!'})
-
-@app.route('/api/getSession', methods=['GET'])
-def getSession():
-    return json.dumps(session['username'])
 
 @app.route('/api/authz/signup/', methods=['POST'])
 def signup():
@@ -52,8 +41,7 @@ def signup():
         data = dbcursor.fetchall()
         if len(data) is 0:
             conn.commit()
-            session['username']=content['username'];
-            return jsonif (content)
+            return json.dumps({'message':'User created successfully !'})
         else:
             return json.dumps({'message':str(data[0])})
     except Exception as e:
@@ -61,70 +49,85 @@ def signup():
 
 @app.route('/api/authz/signin/', methods=['POST'])
 def signin():
-    if not request.json or not 'EmailId' in request.json:
-        return json.dumps({'message':'Error'})
-    content = request.get_json(silent=True)
-    conn=mysql.connect()
-    dbcursor = conn.cursor();
-    dbcursor.callproc('sp_loginUser',(content['EmailId'],0))
-    #data = dbcursor.fetchone()
-    dbcursor.execute('SELECT @_sp_loginUser_1') 
-
-    result = dbcursor.fetchone()
-    if check_password_hash(result[0], content['password']):
-        session['username']=result[1];
-        conn.commit()
-        content['password']=null;
-        return jsonify(content)
-    else:
-        return json.dumps({'message':'Could not log in!!'})
-
-@app.route('/api/authz/logout', methods=['POST'])
-def logout():
-    if 'username' not in session:
-        return json.dumps({'message':'User not logged in!!'})
-    else:
-        session.pop('username', None)
-        return json.dumps({'message':'Could not log in!!'})
-
-@app.route('/api/metadata', methods=['GET'])
-def getmetadata():
     try:
+        if not request.json or not 'EmailId' in request.json and not 'password' in request.json:
+            return json.dumps({'message':'Error'})
         content = request.get_json(silent=True)
         conn=mysql.connect()
         dbcursor = conn.cursor();
-        dbcursor.callproc('sp_getMetadata')
-        data = dbcursor.fetchall()
-        dbcursor.execute('SELECT @_sp_getMetadata_1') 
-        return jsonify(data), 201
-    except Exception as e:
-        pass
+        dbcursor.callproc('sp_loginUser',(content['EmailId'],0,0))
+        dbcursor.execute('SELECT @_sp_loginUser_1,@_sp_loginUser_2') 
+        result = dbcursor.fetchone() 
+        if check_password_hash(result[0], content['password']):
+            conn.commit()
+            return json.dumps({'message':'Logged In'})
+        else:
+            return json.dumps({'message':'Could not log in!!'+str(result[1])})
+    except Exceptions as e:
+        return json.dumps({'message':'Error'})
+
+@app.route('/api/metadata', methods=['GET'])
+def metadata():
+
+   try:
+       content = request.get_json(silent=True)
+       conn=mysql.connect()
+       dbcursor = conn.cursor();
+       dbcursor.callproc('sp_getCityMetadata')
+       #data = dbcursor.fetchone()
+       data = dbcursor.fetchall()
+       items_list1=[]
+       for item in data:
+                i = {
+                    'CityId':item[0],
+                    'CityName':item[1]
+                }
+                items_list1.append(i)
+       #dbcursor.execute('SELECT @_sp_getMetadata_1')
+       dbcursor.callproc('sp_getCategoriesMetadata')
+       data = dbcursor.fetchall()
+       items_list2=[]
+       for item in data:
+                i = {
+                    'CategoryId':item[0],
+                    'CategoryName':item[1]
+                }
+                items_list2.append(i)
+       return json.dumps({'city':items_list1,'category':items_list2})
+   except Exception as e:
+       return json.dumps({'message':'Error'})
+
 
 @app.route('/api/search', methods=['GET'])
 def searchbyfilter():
-    try:
-        category=request.args.get('category');
-        location=request.args.get('location');
-        lookingfor=request.args.get('lookingfor');
-        searchword=request.args.get('searchword');
-        return jsonify({'task': category}), 201
-    except Exception as e:
-        pass
-'''
-@app.route('/api/search/<keyword>', methods=['GET'])
-def searchByCategory(keyword):
+   try:
+       category=request.args.get('category');
+       location=request.args.get('location');
+       user_type=request.args.get('lookingfor');
+       searchword=request.args.get('searchword');
+
+       if location!=None and user_type!=None:
+            
+            return json.dumps({'message':'Error1'})
+       else:
+            return json.dumps({'message':'Error2'})
+
+       return jsonify({'task': category}), 201
+   except Exception as e:
+       return json.dumps({'message':'Error'})
+    
+"""
+@app.route('/api/authz/signin', methods=['GET'])
+def signin():
 
     try:
         if not request.json or not 'title' in request.json:
-            abort(400)
-        content = request.get_json(silent=True)
-        conn=mysql.connect()
-        dbcursor = conn.cursor();
-        dbcursor.callproc('sp_getEventsByCategory',(keyword,0))
+        abort(400)
+        _hashed_password = generate_password_hash(_password)
         return jsonify({'task': task}), 201
     except Exception as e:
-        pass'''
-"""
+        pass
+
 @app.route('/api/authz/init', methods=['GET'])
 def initialize():
 
@@ -155,5 +158,6 @@ def signup():
     except Exception as e:
         pass
 """
-if __name__ == "__main__":         
-        app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
