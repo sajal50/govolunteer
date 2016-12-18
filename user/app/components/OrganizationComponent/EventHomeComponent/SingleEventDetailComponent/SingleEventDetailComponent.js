e547fa866bd0b1a75a9f1d02f4324acd6ddc396f
Sajal Khandelwal <sajal50@gmail.com>
2016-12-18 03:49:33 -0500

    calendar for organization

--- user/app/components/OrganizationComponent/EventHomeComponent/SingleEventDetailComponent/SingleEventDetailComponent.js
+++ user/app/components/OrganizationComponent/EventHomeComponent/SingleEventDetailComponent/SingleEventDetailComponent.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import CSSModules from 'react-css-modules';
 import SingleEventDetailComponentStyle from './assets/SingleEventDetailComponent.scss';
-
+import Modal from 'react-bootstrap/lib/Modal';
+import Button from 'react-bootstrap/lib/Button';
 
 class SingleEventDetailComponent extends React.Component {
 
 	constructor (props) {
 
 		super(props);
+		this.state = {
+			show : false
+		}
 		
 	}
 
@@ -57,42 +61,88 @@ class SingleEventDetailComponent extends React.Component {
 
 	render () {
 
+		// let {eventSelected} = this.props;
+
+		// if (!eventSelected) {
+		// 	return (<div>Click on an event.</div>);
+		// } else {
+
+		// 	let viewForActivities = this.getViewForActivities (this.props.eventSelected.activities);
+
+		// 	return (
+		// 	<div>
+		// 		<div>
+		// 			Title : {eventSelected.title}
+
+		// 		</div>
+		// 		<div>
+		// 			Description : {eventSelected.desc}
+					
+		// 		</div>
+		// 		<div>
+		// 			Start time :  {eventSelected.startTime}
+		// 		</div>
+		// 		<div>
+		// 			End time :  {eventSelected.endTime}
+		// 		</div>
+		// 		<div>
+		// 			Activities : 
+
+		// 			{viewForActivities}
+					
+		// 		</div>
+
+		// 	</div>
+		// 	);
+
+
+		// }
 		let {eventSelected} = this.props;
+		if (eventSelected) {
 
-		if (!eventSelected) {
-			return (<div>Click on an event.</div>);
-		} else {
+		let viewForActivities = this.getViewForActivities (this.props.eventSelected.activities);
 
-			let viewForActivities = this.getViewForActivities (this.props.eventSelected.activities);
 
-			return (
-			<div>
-				<div>
+		return (
+			<Modal show = {this.props.showDetailsModal} 
+			bsSize="large" aria-labelledby="contained-modal-title-lg"
+			onHide={()=> this.props.closeDetailsModal()}>
+		        <Modal.Header closeButton>
+		          <Modal.Title id="contained-modal-title-lg">Event Details</Modal.Title>
+		        </Modal.Header>
+		        <Modal.Body>
+		        	<div>
 					Title : {eventSelected.title}
 
-				</div>
-				<div>
-					Description : {eventSelected.desc}
-					
-				</div>
-				<div>
-					Start time :  {eventSelected.startTime}
-				</div>
-				<div>
-					End time :  {eventSelected.endTime}
-				</div>
-				<div>
-					Activities : 
+					</div>
+					<div>
+						Description : {eventSelected.desc}
+						
+					</div>
+					<div>
+						Start time :  {eventSelected.startTime}
+					</div>
+					<div>
+						End time :  {eventSelected.endTime}
+					</div>
+					<div>
+						Activities : 
 
-					{viewForActivities}
-					
-				</div>
+						{viewForActivities}
+						
+					</div>
+		        </Modal.Body>
+		        <Modal.Footer>
+		          <Button onClick={()=> this.props.closeDetailsModal()}>Close</Button>
+		        </Modal.Footer>
+		    </Modal>
 
-			</div>
 			);
 
 
 		}
+		
+		
 		
 	}
 }
