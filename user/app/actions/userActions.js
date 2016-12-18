172c4b915ab23c6d62529c0df018923679c611a1
Sajal Khandelwal <sajal50@gmail.com>
2016-12-17 23:20:25 -0500

    JSON.stringify for some fetch calls.

--- user/app/actions/userActions.js
+++ user/app/actions/userActions.js
@@ -45,9 +45,9 @@ export function createNewPost (payload) {
 
 			method :'POST',
 			headers: {
-				'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+				'Content-Type': 'application/json'
 			},
-			body: serialize(payload)
+			body: JSON.stringify(payload)
 		}).then((response) => {
 			return response.json();
 		}).then((json)=> {
@@ -84,9 +84,9 @@ export function acceptRequest (payload) {
 
 			method :'POST',
 			headers: {
-				'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+				'Content-Type': 'application/json'
 			},
-			body: serialize(payload)
+			body: JSON.stringify(payload)
 		}).then((response) => {
 			return response.json();
 		}).then((json)=> {
